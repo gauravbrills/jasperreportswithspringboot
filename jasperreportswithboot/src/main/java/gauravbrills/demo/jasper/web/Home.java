a9e1c7cd6c13cfebcb44fdeddab033e168015387
grawat <gauravbrills14@gmail.com>
2015-05-08 09:27:32 +0530

    controller glitch

--- jasperreportswithboot/src/main/java/gauravbrills/demo/jasper/web/Home.java
+++ jasperreportswithboot/src/main/java/gauravbrills/demo/jasper/web/Home.java
@@ -1,6 +1,5 @@
 package gauravbrills.demo.jasper.web;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
